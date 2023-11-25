1bd9d3110ca9aef1e819eaf11e0282f7b0925a5c
Liellison Menezes <liellisonjoseph@gmail.com>
2023-11-25 11:48:35 -0300

    Initial Commit

---
+++ calcmance/calcmanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  calcmanceApp.swift
+//  calcmance
+//
+//  Created by Liellison Menezes on 25/11/23.
+//
+
+import SwiftUI
+
+@main
+struct calcmanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
